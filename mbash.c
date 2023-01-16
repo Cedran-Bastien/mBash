6203dcc86511ed03ac4c3ed6d3247da80d40ed2e
Cedran Bastien <cedran.bastien1@etu.univ-lorraine.fr>
2023-01-16 10:11:08 +0100

    renomage fichier

--- mbash.c
+++ mbash.c
@@ -4,14 +4,26 @@
 #include <unistd.h>
 #include <assert.h>
 #define MAXLI 2048
+
 char cmd[MAXLI];
 char path[MAXLI];
 int pathidx;
 void mbash();
 int main(int argc, char** argv) {
+    static char etat_automate[] =
     while (1) {
         printf("Commande: ");
+
+        //recuperation
         fgets(cmd, MAXLI, stdin);
+        //automate
+        switch (cmd) {
+            case " ":
+                break;
+
+        }
+
+
         mbash(cmd);
     }
     return 0;
