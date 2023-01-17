918ec79f0aabbbb7de980caeb7b6e6dd7a383eb5
Cedran Bastien <cedran.bastien1@etu.univ-lorraine.fr>
2023-01-17 10:43:53 +0100

    debug error segmentation

--- mbash.c
+++ mbash.c
@@ -15,11 +15,11 @@ int pathidx;
 
 void mbash();
 
-int main(int argc, char* argv[],char *envp[]) {
+ int main(int argc, char *argv[], char *envp[]) {
     while (1) {
         printf("Commande: ");
         fgets(cmd, MAXLI, stdin);
-        mbash(cmd);
+        mbash(cmd,envp);
     }
     return 0;
 }
@@ -32,7 +32,7 @@ commande cd, à pouvoir afficher le répertoire courant avec pwd et à lancer
 une commande, comme le fait bash, c’est-à-dire en respectant le contenu de la
 variable PATH.*/
 // Fonction qui permet de lancer une commande en utilisant la variable PATH
-void mbash(char* cmd) {
+void mbash(char* cmd,char *envp[]) {
     // Supprimer le saut de ligne final
     cmd[strlen(cmd) - 1] = '\0';
 
@@ -89,7 +89,7 @@ void mbash(char* cmd) {
     } else {
         pid_t pid = fork();
         if (pid == 0) {
-            execve(path, args, pathEnv);
+            execve(path, args, envp);
             printf("Erreur: Impossible d'exécuter la commande.\n");
         }else {
             // le "&" n'est pas presents
